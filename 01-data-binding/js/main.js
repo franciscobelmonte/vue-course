f4745b4dd64a80623c99c90ffe349823b2585c86
Francisco Belmonte Ruiz <francisco.belmonte.ruiz@gmail.com>
2019-06-17 17:57:49 +0200

    Data binding reactivity with Vanilla Javascript

---
+++ 01-data-binding/js/main.js
@@ -0,0 +1,6 @@
+// Vanilla Javascript
+
+const h1 = document.querySelector('h1');
+const input = document.querySelector('input');
+
+input.addEventListener('keyup', () => h1.innerHTML = input.value);
