d8acec00acaba491b00e3b7b5a3df0bcfc6810e5
Francisco Belmonte Ruiz <francisco.belmonte.ruiz@gmail.com>
2019-08-02 16:06:55 +0200

    Transitions with Vue.js

---
+++ 09-transitions/js/main.js
@@ -0,0 +1,12 @@
+const vm = new Vue({
+    el: 'main',
+    data: {
+        show: true,
+        messages: {
+            transition: 'Transitions with Vue.js',
+            animation: 'Animations with Vue.js',
+            animate: 'Animate.css with Vue.js',
+            elements: 'Transitions between elements with Vue.js'
+        }
+    }
+});
