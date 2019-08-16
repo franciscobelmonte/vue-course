363f9473fa52fb407292f8c9240100e90547de8f
Francisco Belmonte Ruiz <francisco.belmonte.ruiz@gmail.com>
2019-08-16 16:15:22 +0200

    Single file components

---
+++ 3-workflow/23-single-file-components/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <person></person>
+      <person></person>
+      <person></person>
+      <person></person>
+      <person></person>
+  </div>
+</template>
+
+<script>
+import person from './components/Person.vue';
+
+export default {
+  name: 'app',
+  components: {person},
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+</style>
