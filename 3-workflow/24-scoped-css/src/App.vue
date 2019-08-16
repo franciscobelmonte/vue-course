71dd9a7707cb26c6d2d621c35c6b53625a4406ca
Francisco Belmonte Ruiz <francisco.belmonte.ruiz@gmail.com>
2019-08-16 16:45:29 +0200

    Scoped Css

---
+++ 3-workflow/24-scoped-css/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <h1>People</h1>
+      <person></person>
+      <person></person>
+      <person></person>
+      <person></person>
+      <person></person>
+  </div>
+</template>
+
+<script>
+import person from './components/Person.vue';
+
+export default {
+  name: 'app',
+  components: {person},
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ 3-workflow/24-scoped-css/src/components/Person.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <template v-if="person">
+            <h1>{{ personData.name }}</h1>
+            <h2>{{ personData.email }}</h2>
+            <img :src="personData.picture" :alt="personData.name">
+        </template>
+        <span v-else>Loading...</span>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'person',
+  data () {
+    return {
+        person: null
+    }
+  },
+  mounted() {
+      axios.get('https://randomuser.me/api/')
+        .then(response => {
+            this.person = response.data.results[0];
+        })
+  },
+  computed: {
+      personData() {
+          return {
+              name: `${this.person.name.first} ${this.person.name.last}`,
+              picture: this.person.picture.large,
+              email: this.person.email,
+          }
+      }
+  }
+}
+</script>
+
+<style scoped>
+    h1{
+        color: red;
+    }
+</style>
